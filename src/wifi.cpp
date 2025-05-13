ace7b8f6ea50e4b36265e4e923ee2cd0b2b2341f
garekus <garekus@gmail.com>
2025-05-13 17:01:43 +0300

    wifi

---
+++ src/wifi.cpp
@@ -0,0 +1,151 @@
+#include "wifi.h"
+#include "credentials.h"
+
+#define CONNECT_MAX_RETRIES 20
+
+const char *WiFiConnection::config_path = "/wifi.json";
+const WiFiConfig WiFiConnection::defaultConf = {
+    ssid : default_ssid,
+    pwd : default_password,
+    staticIpConfig : {
+        ip : IPAddress(192, 168, 50, 201),
+        gateway : IPAddress(192, 168, 50, 1),
+        subnet : IPAddress(255, 255, 255, 0),
+        dns1 : IPAddress(192, 168, 50, 1)
+    }
+};
+
+void WiFiConnection::loadDefaultConfig()
+{
+    currentConf.ssid = defaultConf.ssid;
+    currentConf.pwd = defaultConf.pwd;
+    currentConf.staticIpConfig.ip = defaultConf.staticIpConfig.ip;
+    currentConf.staticIpConfig.gateway = defaultConf.staticIpConfig.gateway;
+    currentConf.staticIpConfig.subnet = defaultConf.staticIpConfig.subnet;
+    currentConf.staticIpConfig.dns1 = defaultConf.staticIpConfig.dns1;
+}
+
+WiFiError WiFiConnection::init()
+{
+    logger.println("Trying preserved config...");
+    WiFiError res = tryPreservedConfig();
+    if (res == WiFiError::NO_ERROR)
+        return res;
+
+    logger.println("Falling back to hardcoded credentials...");
+    loadDefaultConfig();
+    return connect();
+}
+
+WiFiError WiFiConnection::tryPreservedConfig()
+{
+    FileResult<JsonDocument &> res = fileRepo.readJsonFile(config_path);
+    if (!res.isSuccess())
+    {
+        logger.print("Failed to read config: ");
+        logger.println(res.error());
+        return WiFiError::FILE_CONFIG_ERROR;
+    }
+
+    loadConfigFromJsonDoc(res.value());
+    return connect();
+}
+
+WiFiError WiFiConnection::preserveCurrentConfig()
+{
+    String json;
+    JsonDocument doc;
+
+    doc["ssid"] = currentConf.ssid;
+    doc["pwd"] = currentConf.pwd;
+    doc["staticIp"] = currentConf.staticIpConfig.ip.toString();
+    doc["gateway"] = currentConf.staticIpConfig.gateway.toString();
+    doc["subnet"] = currentConf.staticIpConfig.subnet.toString();
+    doc["dns1"] = currentConf.staticIpConfig.dns1.toString();
+
+    serializeJson(doc, json);
+    FileRepositoryError writeRes = fileRepo.writeJsonFile(config_path, doc);
+    if (writeRes != FileRepositoryError::NO_ERROR)
+    {
+        logger.println("Failed to save config");
+        return WiFiError::CONFIG_SAVE_ERROR;
+    }
+
+    return WiFiError::NO_ERROR;
+}
+
+void WiFiConnection::loadConfigFromJsonDoc(const JsonDocument &doc)
+{
+    currentConf.ssid = doc["ssid"].as<String>();
+    currentConf.pwd = doc["pwd"].as<String>();
+    currentConf.staticIpConfig.ip.fromString(doc["staticIp"].as<String>());
+    currentConf.staticIpConfig.gateway.fromString(doc["gateway"].as<String>());
+    currentConf.staticIpConfig.subnet.fromString(doc["subnet"].as<String>());
+    currentConf.staticIpConfig.dns1.fromString(doc["dns1"].as<String>());
+}
+
+WiFiError WiFiConnection::connect()
+{
+    logger.println("Trying Wi-Fi: ");
+    logger.println(currentConf.ssid.c_str());
+
+    if (currentConf.staticIpConfig.ip.isSet())
+    {
+        WiFi.config(currentConf.staticIpConfig.ip, currentConf.staticIpConfig.gateway, currentConf.staticIpConfig.subnet, currentConf.staticIpConfig.dns1);
+    }
+    WiFi.begin(currentConf.ssid.c_str(), currentConf.pwd.c_str());
+    int retry = 0;
+    while (WiFi.status() != WL_CONNECTED && retry < CONNECT_MAX_RETRIES)
+    {
+        delay(500);
+        logger.print(".");
+        retry++;
+    }
+
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        return WiFiError::NO_ERROR;
+    }
+
+    logger.print("Connection failed with status");
+    logger.println(WiFi.status());
+
+    return WiFiError::CONNECT_ERROR;
+}
+
+WiFiError WiFiConnection::resetFromJson(const String &jsonConfig)
+{
+    if (jsonConfig.isEmpty())
+    {
+        return WiFiError::EMPTY_JSON_ERROR;
+    }
+    JsonDocument jsonConfigDoc;
+    DeserializationError error = deserializeJson(jsonConfigDoc, jsonConfig);
+    if (error)
+    {
+        return WiFiError::DESERIALIZE_ERROR;
+    }
+    loadConfigFromJsonDoc(jsonConfigDoc);
+
+    WiFiError err = connect();
+    if (err != WiFiError::NO_ERROR)
+    {
+        return err;
+    }
+    return preserveCurrentConfig();
+}
+
+String &WiFiConnection::getStatusJson()
+{
+    String json;
+    JsonDocument doc;
+    doc["ssid"] = WiFi.SSID();
+    doc["configured ssid"] = currentConf.ssid;
+    doc["status"] = WiFi.status();
+    doc["rssi"] = WiFi.RSSI();
+    doc["ip"] = WiFi.localIP().toString();
+    doc["mac"] = WiFi.macAddress();
+
+    serializeJson(doc, json);
+    return json;
+}
---
+++ src/wifi.h
@@ -0,0 +1,68 @@
+#ifndef WI_FI_H
+#define WI_FI_H
+
+#include <ESP8266WiFi.h>
+#include "file_repository.h"
+#include "logger.h"
+
+/*
+config json:
+{
+    "ssid": "my_ssid",
+    "pwd": "my_password",
+    "staticIp": "192.168.1.201", // optional
+    "gateway": "192.168.1.1",
+    "subnet": "255.255.255.0",
+    "dns1": "192.168.1.1"
+}
+*/
+
+enum WiFiError
+{
+    NO_ERROR,
+    INIT_ERROR,
+    EMPTY_JSON_ERROR,
+    DESERIALIZE_ERROR,
+    CONNECT_ERROR,
+    CONFIG_SAVE_ERROR,
+    FILE_CONFIG_ERROR
+};
+
+struct IpConfig
+{
+    IPAddress ip;
+    IPAddress gateway;
+    IPAddress subnet;
+    IPAddress dns1;
+};
+
+struct WiFiConfig
+{
+    String ssid;
+    String pwd;
+    IpConfig staticIpConfig;
+};
+
+class WiFiConnection
+{
+private:
+    static const char *config_path;
+    static const WiFiConfig defaultConf;
+    WiFiConfig currentConf;
+    FileRepository &fileRepo;
+    Logger &logger;
+
+    WiFiError preserveCurrentConfig();
+    WiFiError connect();
+    void loadConfigFromJsonDoc(const JsonDocument &doc);
+    void loadDefaultConfig();
+    WiFiError tryPreservedConfig();
+
+public:
+    WiFiConnection(FileRepository &fileRepo, Logger &logger) : fileRepo(fileRepo), logger(logger) {};
+    WiFiError resetFromJson(const String &jsonString);
+    String &getStatusJson();
+    WiFiError init();
+};
+
+#endif
